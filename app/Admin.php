14b242ad6312aaf56a82b44eeabdf3c6274bcc4d
Habibur Rahman <habibblog97@gmail.com>
2023-12-07 20:32:42 +0600

    admin and front class added

---
+++ app/Admin.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Habib\E_Sent_Submit\App;
+
+/**
+ * Admin class
+ */
+class Admin {
+
+    /**
+     * class constructor
+     */
+    public function __construct() {
+        
+    }
+}
---
+++ app/Front.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Habib\E_Sent_Submit\App;
+
+/**
+ * Front class
+ */
+class Front {
+
+    /**
+     * class constructor
+     */
+    public function __construct() {
+        
+    }
+}
