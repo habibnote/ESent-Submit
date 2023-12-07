b3f1c77757bb7ce8ac72c64906ebd548ac3d0ad2
Habibur Rahman <habibblog97@gmail.com>
2023-12-07 21:40:57 +0600

    get admin email address

--- app/Shortcode.php
+++ app/Shortcode.php
@@ -13,6 +13,35 @@ class Shortcode {
     public function __construct() {
         
         add_shortcode( 'ess_main', [$this, 'ess_main_shortcode'] );
+        add_action( 'wp_ajax_ess_form_submit', [$this, 'ess_form_submit'] );
+    }
+
+    /**
+     * Process ajax request
+     */
+    public function ess_form_submit() {
+
+        $ess_name   = $_POST['ess_name'];
+        $ess_email  = $_POST['ess_email'];
+
+        //get admin email address
+        $admin_user = get_users( array( 'role' => 'administrator' ) );
+
+        // Check if there's at least one administrator
+        if ( ! empty( $admin_user ) ) {
+
+            // Get the first administrator's email address
+            $admin_email = get_the_author_meta( 'user_email', $admin_user[0]->ID );
+
+        }
+
+        $to = 'sendto@example.com';
+        $subject = 'The subject';
+        $body = 'The email body content';
+        $headers = array('Content-Type: text/html; charset=UTF-8');
+
+        wp_mail( $to, $subject, $body, $headers );
+
     }
 
     /**
